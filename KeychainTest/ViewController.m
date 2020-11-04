5807dcd6d944e739ac203f634fdb5ef12e76cdca
Seven <seven@email.com>
2020-11-04 18:14:11 +0800

    Commit

--- KeychainTest/ViewController.m
+++ KeychainTest/ViewController.m
@@ -1,13 +1,17 @@
 //
-//  ViewController.m
+//  TestViewController.m
 //  KeychainTest
 //
-//  Created by 黄瑞 on 2020/10/21.
+//  Created by 黄瑞 on 2020/11/4.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *accountField;
+@property (weak, nonatomic) IBOutlet UITextField *labelField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
 
 @end
 
@@ -15,54 +19,73 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // Do any additional setup after loading the view.
 }
 
-- (IBAction)onCheckTap:(UIButton *)sender {
-    NSDictionary *param = @{
-        // 一个典型的查找请求，包含四个部分
-        // 1、kSecClass key，它用来指定搜索对象的类型
-        // 这里是 kSecClassGenericPassword
-        (NSString *)kSecClass: (NSString *)kSecClassGenericPassword,
-        // 2、一个或多个匹配项，这里仅使用账号名称进行匹配
-        // 由于是单用户登录，可以将用户名写死
-        (NSString *)kSecAttrAccount: @"uniqueID",
-        // 3、高级搜索项，例如匹配一个还是所有，是否大小写敏感等
-        // 这里只匹配一个
-        (NSString *)kSecMatchLimit: (NSString *)kSecMatchLimitOne,
-        // 4、指定返回值的类型
-        // 这里需要保存的 Data 也就是用户 token，所以使用
-        (NSString *)kSecReturnData: (NSNumber*)kCFBooleanTrue,
-    };
-    CFTypeRef resType;
-    OSStatus result = SecItemCopyMatching((CFDictionaryRef)param, &resType);
-    
-    NSData *resData = (__bridge NSData *)resType;
-    NSLog(@"resData = %@", [[NSString alloc] initWithData:resData encoding:NSUTF8StringEncoding]);
-    if (result == errSecSuccess) {
-        NSLog(@"errSecSuccess");
-    } else if (result == errSecItemNotFound) {
-        NSLog(@"errSecItemNotFound");
-    } else {
-        NSLog(@"err: %d", (int)result);
-    }
-}
 - (IBAction)onAddTap:(UIButton *)sender {
-    NSDictionary *param = @{
+    NSMutableDictionary *query = [NSMutableDictionary dictionaryWithDictionary:@{
         (NSString *)kSecClass: (NSString *)kSecClassGenericPassword,
-        (NSString *)kSecValueData: [@"123" dataUsingEncoding:NSUTF8StringEncoding],
-        (NSString *)kSecAttrAccount: @"uniqueID",
-    };
-    CFTypeRef res;
-    OSStatus result = SecItemAdd((CFDictionaryRef)param, &res);
-    if (result == errSecSuccess) {
-        NSLog(@"errSecSuccess");
-    } else if (result == errSecDuplicateItem) {
-        NSLog(@"errSecDuplicateItem");
-    } else if (result == errSecItemNotFound) {
-        NSLog(@"errSecItemNotFound");
-    } else {
-        NSLog(@"err: %d", (int)result);
-    }
+    }];
+    if ([self.accountField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrAccount] = self.accountField.text;
+    if ([self.labelField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrLabel] = self.labelField.text;
+    if ([self.valueField.text isEqualToString:@""] == NO) query[(NSString *)kSecValueData] = [self.valueField.text dataUsingEncoding:NSUTF8StringEncoding];
+    
+    OSStatus status = SecItemAdd((CFDictionaryRef)query, NULL);
+    if (status == errSecSuccess) NSLog(@"errSecSuccess");
+    if (status == errSecDuplicateItem) NSLog(@"errSecDuplicateItem");
+    if (status == errSecParam) NSLog(@"errSecParam");
 }
 
+- (IBAction)onDeleteTap:(UIButton *)sender {
+    NSMutableDictionary *query = [NSMutableDictionary dictionaryWithDictionary:@{
+        (NSString *)kSecClass: (NSString *)kSecClassGenericPassword,
+    }];
+    if ([self.accountField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrAccount] = self.accountField.text;
+    if ([self.labelField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrLabel] = self.labelField.text;
+    if ([self.valueField.text isEqualToString:@""] == NO) query[(NSString *)kSecValueData] = [self.valueField.text dataUsingEncoding:NSUTF8StringEncoding];
+
+    OSStatus status = SecItemDelete((CFDictionaryRef)query);
+    if (status == errSecSuccess) NSLog(@"errSecSuccess");
+    if (status == errSecItemNotFound) NSLog(@"errSecItemNotFound");
+    if (status == errSecParam) NSLog(@"errSecParam");
+}
+- (IBAction)onUpdateTap:(UIButton *)sender {
+    NSDictionary *query = @{
+        (NSString *)kSecClass: (NSString *)kSecClassGenericPassword,
+        (NSString *)kSecAttrAccount: @"1",
+    };
+    NSDictionary *update = @{
+        (NSString *)kSecAttrAccount: @"2",
+        (NSString *)kSecValueData: [@"1" dataUsingEncoding:NSUTF8StringEncoding],
+    };
+
+    OSStatus status = SecItemUpdate((CFDictionaryRef)query, (CFDictionaryRef)update);
+    if (status == errSecSuccess) NSLog(@"errSecSuccess");
+    if (status == errSecItemNotFound) NSLog(@"errSecItemNotFound");
+    if (status == errSecDuplicateItem) NSLog(@"errSecDuplicateItem");
+    if (status == errSecParam) NSLog(@"errSecParam");
+
+}
+- (IBAction)onMatchTap:(UIButton *)sender {
+    CFTypeRef result;
+    NSMutableDictionary *query = [NSMutableDictionary dictionaryWithDictionary:@{
+        (NSString *)kSecClass: (NSString *)kSecClassGenericPassword,
+        (NSString *)kSecMatchLimit: (NSString *)kSecMatchLimitOne,
+        (NSString *)kSecReturnData:(NSNumber *)kCFBooleanFalse,
+        (NSString *)kSecReturnAttributes: (NSNumber *)kCFBooleanFalse,
+    }];
+    if ([self.accountField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrAccount] = self.accountField.text;
+    if ([self.labelField.text isEqualToString:@""] == NO) query[(NSString *)kSecAttrLabel] = self.labelField.text;
+    if ([self.valueField.text isEqualToString:@""] == NO) query[(NSString *)kSecValueData] = [self.valueField.text dataUsingEncoding:NSUTF8StringEncoding];
+
+    OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, &result);
+    if (status == errSecSuccess) {
+        NSLog(@"errSecSuccess");
+        NSArray *itemAttr = (__bridge NSArray *)result;
+        NSLog(@"%@", itemAttr);
+    }
+    if (status == errSecItemNotFound) NSLog(@"errSecItemNotFound");
+    if (status == errSecParam) NSLog(@"errSecParam");
+    
+}
 @end
